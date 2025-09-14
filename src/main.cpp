8e5d2ce6b4627443c4bc7e23a0a6a9d2a322f4ec
over <ov3rline@proton.me>
2025-09-14 17:11:00 -0300

    Finish chapter 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,51 @@
 #include "color.h"
+#include "ray.h"
+#include "vec3.h"
 #include <iostream>
 
+Color ray_color(const Ray &r) {
+	Vec3 unit_dir = unit_vector(r.direction());
+	auto a = 0.5 * (unit_dir.y() + 1.0);
+	return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+}
+
 int main() {
-  int image_width = 256;
-  int image_height = 256;
+  // Image //
+
+  auto aspect_ratio = 16.0 / 9.0;
+  int image_width = 400;
+
+  int image_height = int(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
+
+  // Camera //
+
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width = viewport_height * (double(image_width) / image_height);
+  auto camera_center = Point3(0, 0, 0);
+
+  auto viewport_u = Vec3(viewport_width, 0, 0);
+  auto viewport_v = Vec3(0, -viewport_height, 0);
+
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
+
+  auto viewport_upper_left = camera_center - Vec3(0, 0, focal_length) -
+                             viewport_u / 2 - viewport_v / 2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+	// Render //
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
     for (int i = 0; i < image_width; i++) {
-      auto pixel_color = Color(double(i) / (image_width - 1),
-                               double(j) / (image_height - 1), 0);
+      auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_dir = pixel_center - camera_center;
+			auto r = Ray(camera_center, ray_dir);
+
+			Color pixel_color = ray_color(r);
       write_color(std::cout, pixel_color);
     }
   }
