7d3b482e6b7e1c69cfccc5ef23ebfd5269bae572
over <ov3rline@proton.me>
2025-09-30 17:26:52 -0300

    Implementar as esferas

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,35 @@ Color ray_color(const Ray &r) {
     return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
   }
 
+
+  // Grande central.
+  double sphere2 = hit_sphere(Point3(0, 0, -0.3), 0.2, r);
+  if (sphere2 > 0.0) {
+    Vec3 normal = unit_vector(r.at(sphere2) - Vec3(0, 0, -0.3));
+    return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+  }
+
+  // Pequena um pouco à direita.
+  double sphere1 = hit_sphere(Point3(0.3, 0, -0.95), 0.5, r);
+  if (sphere1 > 0.0) {
+    Vec3 normal = unit_vector(r.at(sphere1) - Vec3(0.3, 0, -1.3));
+    return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+  }
+
+  // Distorcida à direita.
+  double sphere3 = hit_sphere(Point3(1, 0.6, -1), 0.5, r);
+  if (sphere3 > 0.0) {
+    Vec3 normal = unit_vector(r.at(sphere3) - Vec3(1, 0.6, -1));
+    return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
+  }
+
   double ground = hit_sphere(Point3(0, -100.5, -1), 100, r);
   if (ground > 0.0) {
     Vec3 normal = unit_vector(r.at(ground) - Vec3(0, -100.5, -1));
     return 0.5 * Color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
   }
 
-	Vec3 unit_dir = unit_vector(r.direction());
+  Vec3 unit_dir = unit_vector(r.direction());
 	auto a = 0.5 * (unit_dir.y() + 1.0);
 	return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
 }
