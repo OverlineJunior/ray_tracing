a2bb8e46d0e956e02634fb2618e38c4c68401d7f
over <ov3rline@proton.me>
2025-10-28 19:49:13 -0300

    Fix outdated parameter naming in Hittable::hit

--- src/hittable.h
+++ src/hittable.h
@@ -20,7 +20,7 @@ class Hittable {
 	public:
 		virtual ~Hittable() = default;
 
-		virtual bool hit(const Ray& r, double ray_tmin, double ray_tmax, HitRecord& rec) const = 0;
+		virtual bool hit(const Ray& r, double min_dist, double max_dist, HitRecord& rec) const = 0;
 };
 
 #endif
